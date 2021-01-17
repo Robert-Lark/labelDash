439e6d3ebc79e1746c09fa87ef620caec9105fa9
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-01-16 18:16:08 -0800

    push for Tivi

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,27 +5,37 @@ function Feed(props) {
   return (
     <div>
       <BodyContainer>
-        <ButtonContainer>
+        <FeedContainer>
           <h1>FEED</h1>
-          <Button>
-            <h3>Your Collection</h3>
-          </Button>
-          <Button>
+          <NewRelease>
+            <div className="title">
+              <h4>New Radio show</h4>
+              <div className="description">
+                <h5>Our latest Radio Show Mixed By DJ DeepField</h5>
+              </div>
+            </div>
+            <div className="content">
+              {" "}
+              <iframe
+                width="100%"
+                height="auto"
+                scrolling="no"
+                frameborder="no"
+                allow="autoplay"
+                src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/335497700&color=%23ff5500&auto_play=false&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=true`}
+              ></iframe>
+            </div>
+          </NewRelease>
+          <NewRadioShow>
             <h3>Request A Title</h3>
-          </Button>
-          <Button>
+          </NewRadioShow>
+          <NewInterview>
             <h3>Reccomendations</h3>
-          </Button>
-          <Button>
+          </NewInterview>
+          <NewMusicVideo>
             <h3>Your Collection</h3>
-          </Button>
-          <Button>
-            <h3>Request A Title</h3>
-          </Button>
-          <Button>
-            <h3>Reccomendations</h3>
-          </Button>
-        </ButtonContainer>
+          </NewMusicVideo>
+        </FeedContainer>
       </BodyContainer>
     </div>
   );
@@ -34,32 +44,68 @@ function Feed(props) {
 export default Feed;
 
 const BodyContainer = styled.div`
-  margin-top: 20%;
-  display: flex;
-  justify-content: space-between;
-  box-shadow: 0px 0px 10px 4px #e0e0e0;
-  border: 1px solid #daf1ff;
+  margin-top: 15%;
 `;
 
-const ButtonContainer = styled.div`
+const FeedContainer = styled.div`
   box-shadow: 0px 0px 10px 4px #e0e0e0;
-  border: 1px solid #daf1ff;
-  height: 90vh;
-  width: 40vw;
-  padding: 50px;
+  height: 95vh;
+  width: 50vw;
+  padding: 30px;
   display: grid;
-  gap: 20px;
+  gap: 10px;
   h1 {
+    height: 50%;
     text-align: center;
   }
 `;
-
-const Button = styled.div`
+const NewRelease = styled.div`
+border-radius: 10px;
+  box-shadow: 0px 0px 10px 4px #e0e0e0;
+  border: 1px solid #daf1ff;
+  display: grid;
+  grid-template-columns: 1fr;
+  gap: 1rem;
+  .title{
+      text-align: center;
+  }
+  .description{
+      margin: 0 10px; 
+      border-top: 1px solid black;
+      margin-bottom: -20px;
+  }
+  .content{
+      height: 80%;
+  }
+`;
+const NewRadioShow = styled.div`
+  box-shadow: 0px 0px 10px 4px #e0e0e0;
+  border: 1px solid #daf1ff;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(100px, 1fr));
+  gap: 1rem;
+`;
+const NewInterview = styled.div`
   box-shadow: 0px 0px 10px 4px #e0e0e0;
   border: 1px solid #daf1ff;
   width: 100%;
   display: flex;
-  flex-direction: column;
+  /* flex-direction: column; */
+  align-items: center;
+  img {
+    border-radius: 20px;
+    max-width: 100%;
+    max-height: 100%;
+    cursor: pointer;
+    padding: 5px;
+  }
+`;
+const NewMusicVideo = styled.div`
+  box-shadow: 0px 0px 10px 4px #e0e0e0;
+  border: 1px solid #daf1ff;
+  width: 100%;
+  display: flex;
+  /* flex-direction: column; */
   align-items: center;
   img {
     border-radius: 20px;
